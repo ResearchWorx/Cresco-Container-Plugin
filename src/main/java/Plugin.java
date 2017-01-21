f1fa9a75241bdbcfdf81a3184557be2896d959c0
Cody Bumgardner <cody@uky.edu>
2017-01-21 09:23:33 -0500

    first commit

--- src/main/java/Plugin.java
+++ src/main/java/Plugin.java
@@ -3,21 +3,28 @@ import com.researchworx.cresco.library.plugin.core.CPlugin;
 
 @AutoService(CPlugin.class)
 public class Plugin extends CPlugin {
+
+    public DockerEngine de;
+
     @Override
     public void setExecutor() {
         setExec(new Executor(this));
     }
 
     public void start() {
-        /*
-         *  Insert your startup code here
-         */
+
+        de = new DockerEngine();
+
+        logger.info("Performance monitoring plugin initialized");
+        perfMonitor = new PerfMonitor(this);
+        perfMonitor.start();
+        setExec(new Executor(this));
     }
 
     @Override
     public void cleanUp() {
-        /*
-         *  Insert your shutdown / clean up code here
-         */
+
+        perfMonitor.stop();
+
     }
 }
