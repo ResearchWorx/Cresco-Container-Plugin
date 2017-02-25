a083e1d7fb621f7b1093940bbbada23502e2e98a
Cody Bumgardner <cody@uky.edu>
2017-02-25 13:26:54 -0500

    rolled back docker-client version due to errors with docker 13.0

--- src/main/java/PerfMonitor.java
+++ src/main/java/PerfMonitor.java
@@ -74,6 +74,11 @@ class PerfMonitor {
             tick.setParam("resource_metric", resourceMetricJSON);
 
             plugin.sendMsgEvent(tick);
+            //double send required to set container resource and get stats... needs to be fixed
+            tick.setParam("resource_id",plugin.getConfig().getStringParam("resource_id","container_resource"));
+            tick.setParam("inode_id",plugin.getConfig().getStringParam("inode_id","container_inode"));
+            plugin.sendMsgEvent(tick);
+
         }
     }
 }
